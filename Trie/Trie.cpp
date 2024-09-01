811f8a1a8f844db9e6905efed3073bc361275d12
Judgement <kjy6135@naver.com>
2024-09-01 14:23:31 +0900

    Docs : 주석추가
    Trie 코드 주석 추가

--- Trie/Trie.cpp
+++ Trie/Trie.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 class Node {
 public:
-	Node* childs[26] = { NULL };
-	bool fin = false;
+	Node* childs[26] = { NULL }; // 자식 노드 초기화
+	bool fin = false; // fin은 해당 단어의 끝
 	Node() {}
 };
 
@@ -18,27 +18,27 @@ public:
 		Node* cur = this->root;
 		for (int i = 0; i < strlen(str); i++) {
 			int idx = str[i] - 'a';
-			if (cur->childs[idx] != NULL)
-				cur = cur->childs[idx];
-			else {
-				cur->childs[idx] = new Node();
-				cur = cur->childs[idx];
+			if (cur->childs[idx] != NULL) // 해당하는 글자가 이미 있을경우
+				cur = cur->childs[idx]; // 이동
+			else { // 없을 경우
+				cur->childs[idx] = new Node(); // 노드 생성
+				cur = cur->childs[idx]; // 이동
 			}
 		}
 
-		cur->fin = true;
+		cur->fin = true; // 해당 단어의 끝을 true로 처리
 	}
 
 	bool find(const char* str) {
 		Node* cur = this->root;
 		for (int i = 0; i < strlen(str); i++) {
 			int idx = str[i] - 'a';
-			if (cur->childs[idx] == NULL) return 0;
-			else cur = cur->childs[idx];
+			if (cur->childs[idx] == NULL) return 0; // 없을 경우
+			else cur = cur->childs[idx]; // 있을 경우 이동
 		}
 
-		if (cur->fin) return 1;
-		return 0;
+		if (cur->fin) return 1; // 해당하는 단어의 fin이 true라면 해당 단어가 있음.
+		return 0; // 없으면 0 반환
 	}
 };
 
